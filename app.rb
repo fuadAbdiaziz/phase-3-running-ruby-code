b7cd4539e427af3aa4d741c45bc7804465e30a26
fuadAbdiaziz <fuadabdaz99@gmail.com>
2023-05-15 12:25:30 -0700

    done

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@
 #     { id: 3, hello: "Moon" }, 
 #     { id: 4, hello: "Learner" }]
 
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
+
+
+
+
+
 # p [1,2,3]
 # puts "Hello World!"
 # print "Pass this test, please."
@@ -14,10 +23,10 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
+# puts ""
+# puts add(1,3) + add(2,2)
